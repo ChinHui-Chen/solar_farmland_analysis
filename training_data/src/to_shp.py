a59318e55b0ff75c2e88757c50e90e1a73766c33
ChinHui-Chen <dongogo@gmail.com>
2016-08-14 17:33:08 +0800

    add merge shp function and add output result

--- training_data/src/to_shp.py
+++ training_data/src/to_shp.py
@@ -7,14 +7,18 @@ import getopt;
 from config import *
 
 def main(argv):
+   prepare_folder()
+
+   # convert kml to shp file
+   kml_to_shp()
+
+   # generate merge shp
+   generate_merge_shp()
+
+
+def kml_to_shp():
    # get tagging folder list
    tagging_folders = next(os.walk(TAGGING_FOLDER))[1]
-
-   # recreate shp folder
-   shutil.rmtree( SHP_FOLDER_T1_LABEL )
-   os.mkdir( SHP_FOLDER_T1_LABEL )
-   shutil.rmtree( SHP_FOLDER_T2T3_NEAR_LABEL )
-   os.mkdir( SHP_FOLDER_T2T3_NEAR_LABEL )
 
    # get kml files in each tagging folder
    for tagging_folder in tagging_folders:
@@ -35,9 +39,47 @@ def main(argv):
             cmd = "%s/ogr2ogr -f 'ESRI Shapefile' %s.shp %s" % (GDAL_PATH, tmp_shp_folder + "/" + kml_file, folder + "/" + kml_file )
             os.system( cmd )
 
-   # generate merge shp
+def generate_merge_shp():
+   # recreate output folder
+   try:
+      shutil.rmtree( OUTPUT_FOLDER )
+   except:
+      pass
+   os.mkdir( OUTPUT_FOLDER )
 
+   # merge
+   merge_shp( T1_LABEL , SHP_FOLDER_T1_LABEL)
+   merge_shp( T2T3_NEAR_LABEL, SHP_FOLDER_T2T3_NEAR_LABEL)
 
+def merge_shp( shp_label, shp_folder ):
+   shp_files = os.listdir( shp_folder )
+   count = 0;
+   for shp_file in shp_files:
+      if ".shp" in shp_file:
+         if count == 0:
+            cmd = "%s/ogr2ogr -f 'ESRI Shapefile' %s.shp %s" % ( GDAL_PATH, OUTPUT_FOLDER + "/" + shp_label, shp_folder + "/" + shp_file )
+            os.system(cmd)
+         else:
+            cmd = "%s/ogr2ogr -f 'ESRI Shapefile' -update -append %s.shp %s -nln %s" % ( GDAL_PATH, OUTPUT_FOLDER + "/" + shp_label, shp_folder + "/" + shp_file, shp_label )
+            os.system(cmd)
+
+         count = count + 1
+
+def prepare_folder():
+   # recreate shp folder
+   try:
+      shutil.rmtree( SHP_FOLDER_T1_LABEL )
+   except:
+      pass
+   os.mkdir( SHP_FOLDER_T1_LABEL )
+
+   try:
+      shutil.rmtree( SHP_FOLDER_T2T3_NEAR_LABEL )
+   except:
+      pass
+   os.mkdir( SHP_FOLDER_T2T3_NEAR_LABEL )
+
+# main
 if __name__ == "__main__":
    main(sys.argv[1:]) 
 
